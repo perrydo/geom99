1250990b957e047562331b2b32b4bb6406b4fc42
perrydo <perrydo@flemingcollege.ca>
2022-04-11 01:25:24 -0400

    testing

--- js/map.js
+++ js/map.js
@@ -71,11 +71,10 @@ var geojsondata5 = new L.GeoJSON.AJAX("treedata.geojson", {
 
 var geojsondata6 = new L.GeoJSON.AJAX("treedata.geojson", {
     filter: function(feature) { return feature.properties.species == "Eastern white pine"},
-    pointToLayer: function(_geoJsonPoint, latlng) {return L.marker(latlng, {icon: greenIcon});},
+    pointToLayer: function(geoJsonPoint, latlng) {return L.marker(latlng, {icon: greenIcon});},
     onEachFeature: function (feature, layer) {
         layer.bindPopup('<b><center> Species: ' + feature.properties.species + '<br>' +
-        '<img src= "https://kc.humanitarianresponse.info/media/original?media_file=perrydo/attachments/'+ 
-        feature.properties.photo + '" style="height:200px;" >' + '<br>' + 'Comments: ' + feature.properties.comments);}}).addTo(coniferous);
+        '<img src= "https://kc.humanitarianresponse.info/media/original?media_file=perrydo/attachments/'+feature.properties.photo + '"' +' style="height:200px;">' + '<br>' + 'Comments: ' + feature.properties.comments);}}).addTo(coniferous);
 
 var geojsondata7 = new L.GeoJSON.AJAX("treedata.geojson", {
     filter: function(feature) { return feature.properties.species == "Jack pine"},
